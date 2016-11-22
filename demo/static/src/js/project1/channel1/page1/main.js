05711a0a6243badf90f1f2e7e9041d6f3b9c8056
mopduan <duanpanzi@163.com>
2016-11-22 16:37:12 +0800

    add demo

---
+++ demo/static/src/js/project1/channel1/page1/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.getElementById('popup').onclick = function () {
+        document.getElementById('mask').style.display = 'block';
+    }
+});
